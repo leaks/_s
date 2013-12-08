0b43548d139c7eb92652b2b61205d68c08656020
Matt Harrison <minus252@gmail.com>
2013-12-08 10:12:48 -0500

    Added missing includes

---
+++ js/pintrest-pinit.js
@@ -0,0 +1,10 @@
+(function(d){
+	var f = d.getElementsByTagName('SCRIPT')[0], p = d.createElement('SCRIPT');
+	p.type = 'text/javascript';
+	p.async = true;
+	p.setAttribute('data-pin-color', 'red');
+	p.setAttribute('data-pin-height', '28');
+	p.setAttribute('data-pin-hover', 'true');
+	p.src = '//assets.pinterest.com/js/pinit.js';
+	f.parentNode.insertBefore(p, f);
+}(document));
